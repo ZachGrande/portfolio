89d0fbf8bfdcef7de3d26d2545cf7deb5199e5f2
Zach Grande <zach.grande23@gmail.com>
2024-04-26 22:35:01 -0700

    Extract card into component

---
+++ app/card.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Card = ({ title, description, imageSrc, siteUrl }: { title: string, description: string, imageSrc: string, siteUrl: string }) => {
+  return (
+    <div className="p-4 m-4 bg-card-brown rounded-xl shadow-lg">
+      <h1 className="flex justify-center mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 sm: text-2xl md:text-3xl lg:text-4xl dark:text-white">
+        {title}
+      </h1>
+      <p className="flex justify-center text-lg font-normal lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+        {description}
+      </p>
+      <div className="flex flex-col justify-center items-center">
+        <div>
+          <img className="max-w-sm mb-6" src={imageSrc} alt="Hockey Teeth" />
+        </div>
+        <a href={siteUrl} className="inline-flex items-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+            Visit the site
+            <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+          </svg>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Card from './card';
+
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
@@ -16,25 +18,12 @@ export default function Home() {
       </header>
       <main className="flex-grow">
         <div className="container mx-auto justify-center">
-          <div className="p-4 m-4 bg-card-brown rounded-xl shadow-lg">
-            <h1 className="flex justify-center mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 sm: text-2xl md:text-3xl lg:text-4xl dark:text-white">
-              Hockey Teeth
-            </h1>
-            <p className="flex justify-center text-lg font-normal lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-              Built with React, hosted with AWS Amplify & EC2.
-            </p>
-            <div className="flex flex-col justify-center items-center">
-              <div>
-                <img className="max-w-sm mb-6" src="/logo.png" alt="Hockey Teeth" />
-              </div>
-              <a href="https://hockeyteethband.com" className="inline-flex items-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                  Visit the site
-                  <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                </svg>
-              </a>
-            </div>
-          </div>
+          <Card
+            title="Hockey Teeth"
+            description="Built with React, hosted with AWS Amplify & EC2."
+            imageSrc="/logo.png"
+            siteUrl="https://hockeyteethband.com"
+          />
         </div>
       </main>
       <footer className="p-4 flex justify-center text-gray-400">&copy; Zach Grande 2024</footer>
