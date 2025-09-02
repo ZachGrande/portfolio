a13430d0e923a17f8fcb381757554250c7d4f8c3
Zach Grande <zach.grande23@gmail.com>
2025-09-01 22:50:22 -0700

    dev: configure lint for react / typescript / nextjs

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,30 @@
 import react from "eslint-plugin-react";
-import { defineConfig, globalIgnores } from "eslint/config";
+import { defineConfig } from "eslint/config";
+import next from "@next/eslint-plugin-next";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
 
 export default defineConfig([
 	{
     files: ["**/*.{js,jsx,ts,tsx}"],
     plugins: {
       react,
+      "@next/next": next,
+      "@typescript-eslint": typescriptEslint,
     },
 		rules: {
+      ...react.configs.recommended.rules,
+      ...next.configs.recommended.rules,
 			semi: "error",
 		},
+    languageOptions: {
+      parser: typescriptParser,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      }
+    }
 	},
   {
     ignores: [
