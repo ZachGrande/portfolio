fd7f192c7e56ebfea8839fb16a64e27532fbc783
Zach Grande <zach.grande23@gmail.com>
2025-09-01 22:58:05 -0700

    fix(lint): ignore react/react-in-jsx-scope (not needed in react 17+)
    https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/react-in-jsx-scope.md#when-not-to-use-it

--- eslint.config.js
+++ eslint.config.js
@@ -14,6 +14,7 @@ export default defineConfig([
     },
 		rules: {
       ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
       ...next.configs.recommended.rules,
 			semi: "error",
 		},
